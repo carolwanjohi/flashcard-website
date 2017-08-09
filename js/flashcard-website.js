79cc70bd7c285680719a550f683ef0827af3fdda
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-08-09 22:13:24 +0300

    Add click fuction to each paragraph heading. Show and hide paragraph after clicking the heading.

--- js/flashcard-website.js
+++ js/flashcard-website.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   $("#java-script").click(function() {
     $("#java-script-text").toggleClass('showMe');
   });
@@ -7,7 +8,73 @@ $(document).ready(function() {
     $("#operators-text").toggleClass('showMe');
   });
 
-  $("#operators").click(function() {
-    $("#operators-text").toggleClass('showMe');
+  $("#variables").click(function() {
+    $("#variables-text").toggleClass('showMe');
   });
+
+  $("#variable-naming-conventions").click(function() {
+    $("#variable-naming-conventions-text").toggleClass('showMe');
+  });
+
+  $("#functions").click(function() {
+    $("#functions-text").toggleClass('showMe');
+  });
+
+  $("#strings").click(function() {
+    $("#strings-text").toggleClass('showMe');
+  });
+
+  $("#methods").click(function() {
+    $("#methods-text").toggleClass('showMe');
+  });
+
+  $("#arguments").click(function() {
+    $("#arguments-text").toggleClass('showMe');
+  });
+
+  $("#parameters").click(function() {
+    $("#parameters-text").toggleClass('showMe');
+  });
+
+  $("#return").click(function() {
+    $("#return-text").toggleClass('showMe');
+  }); 
+
+  $("#changing-methods").click(function() {
+    $("#changing-methods-text").toggleClass('showMe');
+  });
+
+  $("#booleans").click(function() {
+    $("#booleans-text").toggleClass('showMe');
+  });
+
+  $("#undefined").click(function() {
+    $("#undefined-text").toggleClass('showMe');
+  });
+
+  $("#data-NaN").click(function() {
+    $("#data-NaN-text").toggleClass('showMe');
+  });
+
+  $("#escape").click(function() {
+    $("#escape-text").toggleClass('showMe');
+  });
+
+  $("#alert").click(function() {
+    $("#alert-text").toggleClass('showMe');
+  });
+
+  $("#comments").click(function() {
+    $("#comments-text").toggleClass('showMe');
+  });
+
+  $("#jQuery").click(function() {
+    $("#jQuery-text").toggleClass('showMe');
+  });
+
+  $("#attributes").click(function() {
+    $("#attributes-text").toggleClass('showMe');
+  });
+
 });
+
