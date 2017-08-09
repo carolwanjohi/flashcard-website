4b8554c8cbc619a650e982ec30c5402eaed7c1a0
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-08-09 21:13:44 +0300

    Fixing errors

---
+++ js/flashcard-website.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("#java-script").click(function() {
+    $("#java-script-text").toggleClass('showMe');
+  });
+
+  $("#operators").click(function() {
+    $("#operators-text").toggleClass('showMe');
+  });
+
+  $("#operators").click(function() {
+    $("#operators-text").toggleClass('showMe');
+  });
+});
